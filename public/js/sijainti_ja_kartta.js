a8fe170c4789982ce698982e96b9c568f50c2f8b
BUMB4 <tllbumbaforttv@gmail.com>
2020-10-20 18:26:59 +0300

    eka

---
+++ public/js/sijainti_ja_kartta.js
@@ -0,0 +1,47 @@
+var latitude = 0;
+var longitude = 0;
+var olet_tassa;
+var kartta;
+
+if ("geolocation" in navigator) {
+  console.log("Sijaintitieto saatavilla");
+  navigator.geolocation.getCurrentPosition(function(position) {
+    console.log(position);
+
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+
+    console.log('latitude: ' + latitude);
+    console.log('longitude: ' + longitude);
+
+    document.getElementById("pituusasteet").innerHTML = latitude;
+    document.getElementById("leveysasteet").innerHTML = longitude;
+
+    kartta = L.map('kartta').setView([latitude, longitude], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+    }).addTo(kartta);
+
+    var marker = L.marker([latitude, longitude]).addTo(kartta);
+  });
+} else {
+  console.log("Sijaintitiedot ei saatavilla");
+}
+
+function avaa_paikkatietolomake() {
+  document.getElementById("paikkatietolomake").style.display = "block";
+  document.getElementById("lomake").reset();
+}
+
+function sulje_paikkatietolomake() {
+  document.getElementById("paikkatietolomake").style.display = "none";
+}
+
+function laheta_arvostelu() {
+  var paikka = document.getElementById("paikka").value;
+  var arvostelu = document.getElementById("arvostelu").value;
+
+  console.log("Paikka: " + paikka);
+  console.log("Arvostelu: " + arvostelu);
+}
