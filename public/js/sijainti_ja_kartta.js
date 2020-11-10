4c94169b434e91cf087012580dcf61d01d289e37
BUMB4 <tllbumbaforttv@gmail.com>
2020-11-10 19:31:44 +0200

    arvostelun lahetus tehty

--- public/js/sijainti_ja_kartta.js
+++ public/js/sijainti_ja_kartta.js
@@ -31,8 +31,8 @@ function tyhjenna_paikkataulukko() {
   var table = document.getElementById("paikkataulukko");
 
   var rivien_maara = table.rows.length -1;
-  for (var i = 0; rivien_maara; i++) {
-    table.delete(1);
+  for (var i = 0; i < rivien_maara; i++) {
+    table.deleteRow(1);
   }
 }
 
@@ -95,4 +95,29 @@ function laheta_arvostelu() {
 function laheta_arvostelu() {
   var paikka = document.getElementById("paikka").value;
   var arvostelu = document.getElementById("arvostelu").value;
+
+  console.log('laheta arvostelu paikka: ', paikka);
+  console.log('laheta arvostelu arvostelu: ', arvostelu);
+
+  const data = {paikka, arvostelu, longitude, latitude};
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify(data)
+  };
+
+  console.log('laheta arvostelu options: ', options);
+
+  fetch('/api/arvostelu', options).then(function(response) {
+    console.log(response);
+    if(response.status == 200) {
+      tyhjenna_paikkataulukko();
+      getData();
+      sulje_paikkatietolomake();
+    }
+  }, function(error) {
+    console.log(error.message);
+  });
 }
